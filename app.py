f03b01a25042ea6d605de7816609ff88fe9f08f0
Ryan <ryeean16@gmail.com>
2023-10-28 03:29:42 -0700

    menu and contact update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, flash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Choose a random key for your app
@@ -20,31 +20,18 @@ def menu():
     screen_width = request.args.get('width', type=int, default=320)
     session['screen_width'] = screen_width
     menu_items = [
-    {"name": "Espresso", "price": "$3.00", "image": "images/espresso.jpg"},
+    {"name": "Espresso", "price": "$3.00",},
     {"name": "Cappuccino", "price": "$3.50"},
     {"name": "Americano", "price": "$2.50"},
     {"name": "Latte", "price": "$4.00"},
     {"name": "Mocha", "price": "$4.50"},
     {"name": "Macchiato", "price": "$3.75"},
     {"name": "Flat White", "price": "$4.00"},
-    {"name": "Ristretto", "price": "$3.25"},
-    {"name": "Cortado", "price": "$3.75"},
-    {"name": "Doppio", "price": "$3.50"},
     {"name": "Cold Brew", "price": "$4.50"},
     {"name": "Iced Coffee", "price": "$4.00"},
     {"name": "Frappuccino", "price": "$5.00"},
     {"name": "Nitro Cold Brew", "price": "$5.50"},
-    {"name": "Café au Lait", "price": "$3.75"},
     {"name": "Affogato", "price": "$4.25"},
-    {"name": "Viennese Coffee", "price": "$4.50"},
-    {"name": "Turkish Coffee", "price": "$4.00"},
-    {"name": "Café Breve", "price": "$4.25"},
-    {"name": "Café Con Leche", "price": "$3.75"},
-    {"name": "Café Corretto", "price": "$4.50"},
-    {"name": "Café Cubano", "price": "$3.50"},
-    {"name": "Long Black", "price": "$3.00"},
-    {"name": "Lungo", "price": "$3.25"},
-    {"name": "Galão", "price": "$4.00"}
 ]
 
     return render_template('menu.html', items=menu_items, screen_width=screen_width)
@@ -54,6 +41,17 @@ def contact():
     screen_width = request.args.get('width', type=int, default=320)
     session['screen_width'] = screen_width
     return render_template('contact.html', screen_width=screen_width)
+
+@app.route('/submit_contact', methods=["POST"])
+def submit_contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    # TODO: Handle the form data (e.g., save it to a database, send an email, etc.)
+    # For now, just flash a success message
+    flash("Thanks for reaching out, {}! We'll get back to you soon.".format(name), 'success')
+    return redirect(url_for('contact'))
 
 @app.route('/buynow')
 def buynow():
