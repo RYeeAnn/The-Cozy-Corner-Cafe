086de1ccc3d4694b9e11d412b1093393f881ea31
Ryan <ryeean16@gmail.com>
2023-11-01 21:57:36 -0700

    footer added, about section added :fire:

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,12 +1,11 @@
-// This goes inside your menu.js file
 
 // Search function
 document.getElementById("menuSearch").addEventListener("keyup", function() {
     // Get the value of the input field
-    var query = this.value.toLowerCase();
+    const query = this.value.toLowerCase();
     
     // Get all menu item names
-    var items = document.querySelectorAll(".menu__item-name");
+    const items = document.querySelectorAll(".menu__item-name");
 
     // Loop through all items and hide those that don't match the search query
     items.forEach(function(item) {
@@ -20,20 +19,14 @@ document.getElementById("menuSearch").addEventListener("keyup", function() {
 
 
 
-
-
-
-
-
-
 // Get modal and list items
-var modal = document.getElementById("modal");
-var listItems = document.querySelectorAll(".menu__list > li");
+const modal = document.getElementById("modal");
+const listItems = document.querySelectorAll(".menu__list > li");
 
 // When the user clicks on a list item, open the modal
 listItems.forEach(item => {
     item.onclick = function() {
-        var btn = item.querySelector(".menu__item-name");
+        const btn = item.querySelector(".menu__item-name");
         modal.style.display = "block";
         document.getElementById("modalName").textContent = btn.getAttribute("data-modal-trigger");
         document.getElementById("modalDescription").textContent = btn.getAttribute("data-description");
@@ -43,7 +36,7 @@ listItems.forEach(item => {
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
